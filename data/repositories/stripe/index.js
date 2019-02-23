6ef012c8a9a8e8d269b4faf5c180b1f7f454ce9f
DimosthenisBotsaris <botsaris.d@gmail.com>
2019-02-23 14:06:12 +0200

    add stripe repo

--- data/repositories/stripe/index.js
+++ data/repositories/stripe/index.js
@@ -82,19 +82,12 @@ const paymentsInterface = {
       limit,
       source,
     }) {
-      try {
-        const charge = await client.charges.list(getFieldsToListCharges({
-          created,
-          customer,
-          limit,
-          source,
-        }));
-        return {
-          data: charge,
-        };
-      } catch (error) {
-        return handleStripeApiErrors(error);
-      }
+      return client.charges.list(getFieldsToListCharges({
+        created,
+        customer,
+        limit,
+        source,
+      }));
     },
 
     async create({
@@ -106,33 +99,19 @@ const paymentsInterface = {
       description,
       metadata,
     }) {
-      try {
-        const charge = await client.charges.create(removeUndefinedValues({
-          amount,
-          currency,
-          capture,
-          receipt_email,
-          customer,
-          description,
-          metadata,
-        }));
-        return {
-          data: charge,
-        };
-      } catch (error) {
-        return handleStripeApiErrors(error);
-      }
+      return client.charges.create(removeUndefinedValues({
+        amount,
+        currency,
+        capture,
+        receipt_email,
+        customer,
+        description,
+        metadata,
+      }));
     },
 
     async get(chargeId) {
-      try {
-        const charge = await client.charges.retrieve(chargeId);
-        return {
-          data: charge,
-        };
-      } catch (error) {
-        return handleStripeApiErrors(error);
-      }
+      return client.charges.retrieve(chargeId);
     },
 
     async update({
@@ -140,31 +119,17 @@ const paymentsInterface = {
       customer,
       description,
     }) {
-      try {
-        const charge = await client.charges.update(
-          chargeId,
-          getFieldsToUpdateCharge({
-            customer,
-            description,
-          }),
-        );
-        return {
-          data: charge,
-        };
-      } catch (error) {
-        return handleStripeApiErrors(error);
-      }
+      return client.charges.update(
+        chargeId,
+        getFieldsToUpdateCharge({
+          customer,
+          description,
+        }),
+      );
     },
 
     async capture(chargeId) {
-      try {
-        const charge = await client.charges.capture(chargeId);
-        return {
-          data: charge,
-        };
-      } catch (error) {
-        return handleStripeApiErrors(error);
-      }
+      return client.charges.capture(chargeId);
     },
   },
 
@@ -174,46 +139,25 @@ const paymentsInterface = {
       email,
       limit,
     }) {
-      try {
-        const charge = await client.customers.list({
-          created,
-          email,
-          limit,
-        });
-        return {
-          data: charge,
-        };
-      } catch (error) {
-        return handleStripeApiErrors(error);
-      }
+      return client.customers.list({
+        created,
+        email,
+        limit,
+      });
     },
 
     async create({
       source,
       email,
     }) {
-      try {
-        const customer = await client.customers.create(removeUndefinedValues({
-          email,
-          source,
-        }));
-        return {
-          data: customer,
-        };
-      } catch (error) {
-        return handleStripeApiErrors(error);
-      }
+      return client.customers.create(removeUndefinedValues({
+        email,
+        source,
+      }));
     },
 
     async get(customerId) {
-      try {
-        const customer = await client.customers.retrieve(customerId);
-        return {
-          data: customer,
-        };
-      } catch (error) {
-        return handleStripeApiErrors(error);
-      }
+      return client.customers.retrieve(customerId);
     },
 
     async update({
@@ -222,32 +166,18 @@ const paymentsInterface = {
       email,
       source,
     }) {
-      try {
-        const customer = await client.customers.update(
-          customerId,
-          {
-            description,
-            email,
-            source,
-          },
-        );
-        return {
-          data: customer,
-        };
-      } catch (error) {
-        return handleStripeApiErrors(error);
-      }
+      return client.customers.update(
+        customerId,
+        {
+          description,
+          email,
+          source,
+        },
+      );
     },
 
     async delete(customerId) {
-      try {
-        const customer = await client.customers.del(customerId);
-        return {
-          data: customer,
-        };
-      } catch (error) {
-        return handleStripeApiErrors(error);
-      }
+      return client.customers.del(customerId);
     },
   },
 
@@ -256,32 +186,18 @@ const paymentsInterface = {
       refundId,
       amount,
     }) {
-      try {
-        const refund = amount
-          ? await client.refunds.create({
-            charge: refundId,
-            amount,
-          })
-          : await client.refunds.create({
-            charge: refundId,
-          });
-        return {
-          data: refund,
-        };
-      } catch (error) {
-        return handleStripeApiErrors(error);
-      }
+      return amount
+        ? client.refunds.create({
+          charge: refundId,
+          amount,
+        })
+        : client.refunds.create({
+          charge: refundId,
+        });
     },
 
     async get(refundId) {
-      try {
-        const refund = await client.refunds.retrieve(refundId);
-        return {
-          data: refund,
-        };
-      } catch (error) {
-        return handleStripeApiErrors(error);
-      }
+      return client.refunds.retrieve(refundId);
     },
   },
 };
